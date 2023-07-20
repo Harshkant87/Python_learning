2c9c43a0aa3ce794807f17c52d14f38edd88bef3
Harshkant87 <harshkant87@gmail.com>
2023-07-20 23:53:12 +0530

    color_producer_function add

--- 11.Web_mapping_project/map_process.py
+++ 11.Web_mapping_project/map_process.py
@@ -9,8 +9,14 @@ longitudes = list(data['Longitude'])
 places = list(data['Place'])
 names = list(data['Name'])
 
+def color_producer(longitude):
+    if longitude < 80.0:
+        return 'red'
+    else:
+        return 'green'
+
 for i in range(len(names)):
-    fg.add_child(folium.Marker(location = [latitudes[i], longitudes[i]], popup = names[i] + "," + places[i] ,  icon = folium.Icon(color = "green")))
+    fg.add_child(folium.Marker(location = [latitudes[i], longitudes[i]], popup = names[i] + "," + places[i] ,  icon = folium.Icon(color = color_producer(longitudes[i]))))
 
 map.add_child(fg)
 map.save("national_parks_map.html")
