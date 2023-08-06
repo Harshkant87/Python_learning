5dd6d5648db8dc086d9bb0c05a3bee97bbc7426f
Harshkant87 <harshkant87@gmail.com>
2023-08-06 14:44:27 +0530

    Thesaurus v2

--- 12.English_thesaurus_project/app.py
+++ 12.English_thesaurus_project/app.py
@@ -1,10 +1,24 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def translate(word):
-    result = data[word.upper()]
-    return result
+    word = word.upper()
+    if word in data:
+        result = data[word]
+        return result
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
+
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist please double check it!"
+        else:
+            return "Kindly enter valid input!"
+    else:
+        return "The word does not exist!"
 
 word = input("Enter word: ")
 print(translate(word))
