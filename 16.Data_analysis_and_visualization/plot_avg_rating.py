3ee77ebf5034ab454ee0a95160d62f641195ecab
Harshkant87 <harshkant87@gmail.com>
2023-09-13 15:27:31 +0530

    Downsize plot from date to week

--- 16.Data_analysis_and_visualization/plot_avg_rating.py
+++ 16.Data_analysis_and_visualization/plot_avg_rating.py
@@ -8,8 +8,14 @@ data = pandas.read_csv("course_reviews.csv", parse_dates=['Timestamp'])
 data['Day'] = data['Timestamp'].dt.date
 day_avg = data.groupby(['Day']).mean(numeric_only = True) #grouping according to day
 # print(day_avg.index)
-plt.figure(figsize = (25, 3))
-plt.plot(day_avg.index, day_avg['Rating']) #placing X and Y here
-plt.show()
+# plt.figure(figsize = (25, 3))
+# plt.plot(day_avg.index, day_avg['Rating']) #placing X and Y here
+# plt.show()
+data['Week'] = data['Timestamp'].dt.strftime('%Y-%U') #for month n week use %m-%U
+week_avg = data.groupby(['Week']).mean(numeric_only = True)
+# print(week_avg['Rating'])
 
+plt.figure(figsize = (15, 3))
+plt.plot(week_avg.index, week_avg['Rating'])
+plt.show()
 
