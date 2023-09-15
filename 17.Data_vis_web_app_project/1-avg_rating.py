c07fb82a01de3882a97f16ea93397e2e870dc9e8
Harshkant87 <harshkant87@gmail.com>
2023-09-15 15:22:42 +0530

    high-chart and justpy

---
+++ 17.Data_vis_web_app_project/1-avg_rating.py
@@ -0,0 +1,69 @@
+import justpy as jp
+import pandas
+from datetime import datetime
+from pytz import utc
+import matplotlib.pyplot as plt
+
+data = pandas.read_csv(r"C:\python_learning\16.Data_analysis_and_visualization\course_reviews.csv", parse_dates=['Timestamp'])
+data['Day'] = data['Timestamp'].dt.date
+day_avg = data.groupby(['Day']).mean(numeric_only = True)
+
+chart_def = '''
+{
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Corn vs wheat estimated production for 2020',
+        align: 'left'
+    },
+    subtitle: {
+        text:'Source: <a target = "_blank", href="https://www.indexmundi.com/agriculture/?commodity=corn">indexmundi</a>',
+        align: 'left'
+    },
+    xAxis: {
+        categories: ['USA', 'China', 'Brazil', 'EU', 'India', 'Russia'],
+        crosshair: true,
+        accessibility: {
+            description: 'Countries'
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: '1000 metric tons (MT)'
+        }
+    },
+    tooltip: {
+        valueSuffix: ' (1000 MT)'
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [
+        {
+            name: 'Corn',
+            data: [406292, 260000, 107000, 68300, 27500, 14500]
+        },
+        {
+            name: 'Wheat',
+            data: [51086, 136000, 5500, 141000, 107180, 77000]
+        }
+    ]
+}
+'''
+
+def app():
+    wp = jp.QuasarPage()
+    h1 = jp.QDiv(a = wp, text = "Analysis of course reviews", classes = "text-h3 text-center q-pa-md")
+    p1 = jp.QDiv(a = wp, text = "These graphs represent course review analysis")
+    hc = jp.HighCharts(a = wp, options = chart_def)
+
+    # print(hc.options.series)
+
+    return wp
+
+jp.justpy(app)
