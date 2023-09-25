d49380153534817dbf6a7cafdf0e6b4cc48bde6a
Harshkant87 <harshkant87@gmail.com>
2023-09-25 16:41:15 +0530

    rendering html file through flask

--- 18.Flask_project/script1.py
+++ 18.Flask_project/script1.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/') #decorator
 def home():
-    return "Welcome to my first Flask demo!"
+    return render_template("home.html") #html file should be under templates folder
 
 @app.route('/about/') #decorator
 def about():
-    return "This is about section"
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
