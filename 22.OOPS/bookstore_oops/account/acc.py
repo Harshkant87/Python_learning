d3ec83e46b0f4fb5c112e4cbba3893813205fff3
Harshkant87 <harshkant87@gmail.com>
2023-10-16 19:33:35 +0530

    Inheritance

--- 22.OOPS/bookstore_oops/account/acc.py
+++ 22.OOPS/bookstore_oops/account/acc.py
@@ -19,10 +19,26 @@ class Account:
             file.write(str(self.balance))
 
 
-harsh_account = Account("account//balance.txt")
-print(harsh_account.balance)
-harsh_account.withdraw(90)
-print(harsh_account.balance)
-harsh_account.deposit(100)
-print(harsh_account.balance)
-harsh_account.commit()
+class Checking(Account): #base class passed as argument this is inheritance
+    def __init__(self, filepath, fee):
+        self.fee = fee
+        Account.__init__(self, filepath)
+    
+    def transfer(self, amount):
+        self.balance = self.balance - amount - self.fee
+
+
+checking = Checking("account//balance.txt", 10)
+checking.deposit(100)
+print(checking.balance)
+checking.transfer(100)
+checking.commit()
+print(checking.balance)
+
+# harsh_account = Account("account//balance.txt")
+# print(harsh_account.balance)
+# harsh_account.withdraw(90)
+# print(harsh_account.balance)
+# harsh_account.deposit(100)
+# print(harsh_account.balance)
+# harsh_account.commit()
